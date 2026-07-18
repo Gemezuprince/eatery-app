a5255280a859aed6876bbe43c019b72c33e08832
Gemezuprince <gemezuprince1999@gmail.com>
2026-07-18 13:29:55 +0100

    fix: remove unused next callback from async Mongoose pre-hooks

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -50,22 +50,20 @@ const userSchema = new mongoose.Schema(
   }
 );
 
-userSchema.pre('save', async function (next) {
+userSchema.pre('save', async function () {
   if (!this.isModified('password')) {
-    return next();
+    return;
   }
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
-  next();
 });
 
-userSchema.pre('findOneAndUpdate', async function (next) {
+userSchema.pre('findOneAndUpdate', async function () {
   const update = this.getUpdate();
   if (update.password) {
     const salt = await bcrypt.genSalt(10);
     update.password = await bcrypt.hash(update.password, salt);
   }
-  next();
 });
 
 userSchema.methods.matchPassword = async function (enteredPassword) {
